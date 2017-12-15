e4845791806f25a306a13cfa542c8d225f0268b8
Phillip Lehner <pl@philliplehner.com>
2017-12-15 11:05:49 -0600

    New test

--- mouse.c
+++ mouse.c
@@ -1,25 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-
 #include <linux/input.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
 
-#define MOUSEFILE "/dev/input/event0"
-
-int main()
+int main(int argc, char **argv)
 {
-	int fd;
-	struct input_event ie;
+    int fd;
+    if(argc < 2) {
+        printf("usage: %s \n", argv[0]);
+        return 1;
+    }
+    fd = open(argv[1], O_RDONLY);
+    struct input_event ev;
 
-	if((fd = open(MOUSEFILE, O_RDONLY)) == -1) {
-		perror("opening device");
-		exit(EXIT_FAILURE);
-	}
+    while (1)
+    {
+        read(fd, &ev, sizeof(struct input_event));
 
-	while(read(fd, &ie, sizeof(struct input_event))) {
-		printf("time %ld.%06ld\ttype %d\tcode %d\tvalue %d\n",
-		       ie.time.tv_sec, ie.time.tv_usec, ie.type, ie.code, ie.value);
-	}
-
-	return 0;
+        if(ev.type == 1)
+            //printf("key %i state %i\n", ev.code, ev.value);
+            if(ev.value == 0)
+                printf(" : [key %i]\n ", ev.code);
+    }
 }
