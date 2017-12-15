cfb044186e81e1755ee968fcf2d5723e20114680
Phillip Lehner <pl@philliplehner.com>
2017-12-15 18:14:36 +0000

    This tests and works

--- mouse.c
+++ mouse.c
@@ -1,63 +1,42 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <linux/input.h>
 #include <unistd.h>
+#include <fcntl.h>
 
-#define MOUSEFILE "/dev/input/event0"
-//
-int main()
+int main(int argc, char** argv)
 {
-    int fd;
-    struct input_event ie;
-    unsigned char *ptr = (unsigned char*)&ie;
-    //
-    unsigned char button,bLeft,bMiddle,bRight;
-    char x,y;                                                            // the relX , relY datas
-    int absolute_x,absolute_y;
+    int fd, bytes;
+    unsigned char data[3];
 
-    if((fd = open(MOUSEFILE, O_RDONLY | O_NONBLOCK )) == -1)
+    const char *pDevice = "/dev/input/mice";
+
+    // Open Mouse
+    fd = open(pDevice, O_RDWR);
+    if(fd == -1)
     {
-        printf("NonBlocking %s open ERROR\n",MOUSEFILE);
-        exit(EXIT_FAILURE);
+        printf("ERROR Opening %s\n", pDevice);
+        return -1;
     }
-    else
-    {
-        printf("NonBlocking %s open OK\n",MOUSEFILE);
-    }
-    //
-    printf("--------------------------------------------------------\n");
-    printf("right-click to set absolute x,y coordinates origin (0,0)\n");
-    printf("left+right click to EXIT\n");
-    printf("--------------------------------------------------------\n");
-    //
+
+    int left, middle, right, x, y;
+    signed char xd, yd;
     while(1)
-    {       
-        if(read(fd, &ie, sizeof(struct input_event))!=-1)
+    {
+        // Read Mouse     
+        bytes = read(fd, data, sizeof(data));
+
+        if(bytes > 0)
         {
-            //
-            button=ptr[0];
-            bLeft = button & 0x1;
-            bMiddle = ( button & 0x4 ) > 0;
-            bRight = ( button & 0x2 ) > 0;
-            x=(char) ptr[1];y=(char) ptr[2];
-            if(bLeft&bRight) { printf("\nright+left ,EXIT\n");break;}
-            // computes absolute x,y coordinates
-            absolute_x+=x;
-            absolute_y-=y;
-            // set absolute reference ?
-            if (bRight==1)
-            {
-                absolute_x=0;
-                absolute_y=0;
-                printf("Absolute x,y coords origin recorded\n");
-            }
-            // show it!
-            printf("X%i Y%i\n",absolute_x,absolute_y);
-            fflush(stdout);
-        }
+            left = data[0] & 0x1;
+            right = data[0] & 0x2;
+            middle = data[0] & 0x4;
+
+            xd = data[1];
+            yd = data[2];
+            x += xd;
+            y += yd;
+
+            printf("x=%d, y=%d, x-DELTA=%d, y-DELTA=%d, \n", x, y, xd, yd);
+        }   
     }
-    //
-    close(fd);
-return 0;
+    return 0; 
 }
